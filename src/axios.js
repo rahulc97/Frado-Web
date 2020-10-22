9b469ea127ae0d6542b02954d9c327b31b5d2f68
rahulc97 <gorahul10@gmail.com>
2020-10-22 17:54:05 +0530

    heroku hosted endpoint

--- src/axios.js
+++ src/axios.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    // baseURL: 'https://reboothack12345.herokuapp.com',
-    baseURL: 'http://localhost:8000'
-
+  // baseURL: 'https://reboothack12345.herokuapp.com',
+  // baseURL: 'http://localhost:8000'
+  baseURL: 'https://fradoacademyendpoints.herokuapp.com',
 });
 
 export default instance;
